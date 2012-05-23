a03086663b4506dec90e7bdffb99e2fbbc2c5cae
brian-butcher <brian.butcher@sofiac.us>
2012-05-23 16:09:28 -0700

    implemented the getPropertyValue function

--- src/main/java/org/bluemagic/config/service/dao/impl/PropertiesDaoJdbcImpl.java
+++ src/main/java/org/bluemagic/config/service/dao/impl/PropertiesDaoJdbcImpl.java
@@ -6,9 +6,18 @@ import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 public class PropertiesDaoJdbcImpl extends JdbcDaoSupport implements PropertiesDao {
 
+	private static final String SELECT_PROPERTY_VALUE = "SELECT VALUE FROM PROPERTIES WHERE PROPERTYKEY=?";
+
 	@Override
 	public String getPropertyValue(String propertyKey){
 
-		return "";
+		try{
+			// Try to select the propertyKey from the table. If the propertyKey is not found, the result will be null.
+			return getJdbcTemplate().queryForObject(SELECT_PROPERTY_VALUE, new Object[] { propertyKey }, String.class);
+		} catch {
+
+			// Means the propertyKey did not exist.
+			return null;
+		}
 	}
 }
