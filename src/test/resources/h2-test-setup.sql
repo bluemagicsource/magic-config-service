8a04c50dbf97fef89a62e0669fe63f0365de3f80
brian-butcher <brian.butcher@sofiac.us>
2012-05-23 17:28:06 -0700

    added code to h2-test-setup.sql to create a properties table

--- src/test/resources/h2-test-setup.sql
+++ src/test/resources/h2-test-setup.sql
@@ -12,3 +12,19 @@ ID identity,
 KEY varchar(256),
 VALUE varchar(256),
 constraint PK_TAG_ID primary key (ID));
+
+drop table if exists PROPERTIES;
+create table PROPERTIES (
+ID identity,
+KEY varchar(256),
+VALUE varchar(256),
+VERSION int,
+CREATION_USER varchar(256),
+CREATION_DATETIME timestamp,
+ODOMETER int,
+LAST_ACCESSED_DATETIME timestamp,
+LAST_ACCESSED_USER varchar(256),
+LAST_MODIFIED_DATETIME timestamp,
+LAST_MODIFIED_USER varchar(256),
+constraint PK_PROP_ID primary key (ID),
+constraint UC_PROP_KEY unique (VALUE));
