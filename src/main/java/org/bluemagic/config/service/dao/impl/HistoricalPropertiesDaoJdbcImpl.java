43181a34557314193d2b22d658fb487180c2a2fb
bossross88 <richardcarroll88@gmail.com>
2012-06-02 03:51:05 -0700

    created Historical Properties Dao

---
+++ src/main/java/org/bluemagic/config/service/dao/impl/HistoricalPropertiesDaoJdbcImpl.java
@@ -0,0 +1,46 @@
+package org.bluemagic.config.service.dao.impl;
+
+import org.bluemagic.config.api.service.CompletePropertyDetails;
+import org.bluemagic.config.api.service.PropertyDetails;
+import org.bluemagic.config.service.dao.HistoricalPropertiesDao;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class HistoricalPropertiesDaoJdbcImpl extends JdbcDaoSupport implements HistoricalPropertiesDao {
+
+	private static final String INSERT_PROPERTY_VALUE = "INSERT INTO HISTORICAL PROPERTIES (KEY, VALUE) VALUES (?,?)";
+
+        @Override
+	public boolean insertProperty(String propertyKey, String propertyValue) {
+	    
+	    int rowsUpdated = getJdbcTemplate().update(INSERT_PROPERTY_VALUE, propertyKey, propertyValue);
+	    if (rowsUpdated == 1) {
+		return true;
+	    } else {
+		return false;
+	    }
+	}
+
+	@Override
+	public String getPropertyValue(String propertyKey){
+
+		try{
+			// Try to select the propertyKey from the table. If the propertyKey is not found, the result will be null.
+			return getJdbcTemplate().queryForObject(SELECT_PROPERTY_VALUE, new Object[] { propertyKey }, String.class);
+		} catch (Throwable t) {
+
+			// Means the propertyKey did not exist.
+			return null;
+		}
+	}
+
+	@Override
+	public PropertyDetails getPropertyDetails(String propertyWithTags) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public CompletePropertyDetails getCompletePropertyDetails(String propertyWithTags) {
+		throw new UnsupportedOperationException();
+	}
+}
