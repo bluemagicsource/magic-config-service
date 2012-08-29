d9b067ad18b1cf980ff05e47a1d2cddcff4b804c
Danny Bouman <dannybb@gmail.com>
2012-08-29 12:47:58 -0400

    Moved h2 sql files into classpath and added code to initialize database into spring config

--- src/main/resources/h2-setup.sql
+++ src/main/resources/h2-setup.sql
@@ -1,18 +1,18 @@
-create table USERS (
+create table if not exists USERS (
 ID identity,
 USERNAME varchar(256),
 CREATION_USER varchar(256) DEFAULT 'System',
 CREATION_DATETIME timestamp,
 constraint PK_USER_ID primary key (ID));
 
-create table TAGS (
+create table if not exists TAGS (
 ID identity,
 KEY varchar(256),
 VALUE varchar(256),
 TYPE varchar(256),
 constraint PK_TAG_ID primary key (ID));
 
-create table PROPERTIES (
+create table if not exists PROPERTIES (
 ID identity,
 KEY varchar(256),
 VALUE varchar(256),
@@ -27,7 +27,7 @@ LAST_MODIFIED_USER varchar(256),
 constraint PK_PROP_ID primary key (ID),
 constraint UC_PROP_KEY unique (VALUE));
 
-create table HISTORICAL_PROPERTIES (
+create table if not exists HISTORICAL_PROPERTIES (
 ID bigint,
 KEY varchar(256),
 VALUE varchar(256),
@@ -39,7 +39,7 @@ LAST_ACCESSED_USER varchar(256),
 LAST_MODIFIED_DATETIME timestamp,
 LAST_MODIFIED_USER varchar(256));
 
-create table PROPERTIES_TAG_MAPPING (
+create table if not exists PROPERTIES_TAG_MAPPING (
 PROPERTY_ID bigint,
 TAG_ID bigint,
 constraint PK_PROPTAG_ID primary key (PROPERTY_ID, TAG_ID),
